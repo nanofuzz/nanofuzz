82ad63281571a9e4564593adcb35d46f8cfe01fe
Matt Davis <22134678+cmumatt@users.noreply.github.com>
2025-02-10 09:03:25 -0500

    feat: provenance of inputs & output values

--- src/llm/ProgramModelFactory.ts
+++ src/llm/ProgramModelFactory.ts
@@ -6,27 +6,31 @@ import { FunctionDef } from "fuzzer/Fuzzer";
 /** !!!!!! TODO !!!!!!
  *
  * Features:
- * - NoInput should be respected in suggested tests (e.g., unions)
+ * - Suggest expected output
+ * - Suggested tests should respect NoOutput (i.e., unions, objects)
+ * - More options setting to turn AI-generated tests on/off
  * - Statistics about number of tests suggested (i)
  * - Telemetry logging for model queries
  * - CodeLens to insert generated specification if missing
  * - Support at least one other model, e.g., Copilot
+ * - Experiment w/problems tab
  *
  * Fix:
- * - Suggested tests should not bypass the fuzz counter like saved tests
- * - Retry/fail logic for Gemini back-end failures (e.g., overloaded)
- * - Missing tests for models, specs
+ * - Validate incoming model tests
+ * - Schemas for literal unions do not look right:
+ *
+ * - Retry/fail logic for model back-end failures (e.g., overloaded)
+ * - Add tests for models, specs
+ * - Use real prompt parameterization
+ * - Prompt cache invalidation & hash key
  *
  * Could be better:
  * - _getSuggestedInputs() should return test cases not inputs
- * - Analyze spec during CodeLens analysis phase
+ * - Analyze spec during CodeLens analysis phase (perf)
  * - Check injected tests to ensure they meet the requirements
  * - Pipeline & sequence queries by concrete implementation
- * - Use real prompt parameterization
- * - Prompt cache invalidation
  * - Singleton concrete models by model, module, fnref
- * - We really need to have just one override format, not two
- * -
+ * - Would benefit from having one override format, not two
  */
 export abstract class ProgramModelFactory {
   public static create(fn: FunctionDef): AbstractProgramModel {
