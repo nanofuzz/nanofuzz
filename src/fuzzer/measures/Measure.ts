c1bec79d3dd0e8f9a9ca9ecf93936b268e58a0ad
Tai Sanh Nguyen <tai.sanh.ng@gmail.com>
2025-07-28 14:58:24 -0400

    chore: remove comments

--- src/fuzzer/measures/Measure.ts
+++ src/fuzzer/measures/Measure.ts
@@ -1,19 +1,5 @@
 import { FuzzTestResults } from "fuzzer/Fuzzer";
 
-// export interface Measure {
-//   /** Name for logging/debugging. */
-//   readonly name: string;
-
-//   /** How important this measure is relative to others. Dynamically changes. */
-//   weight: number;
-
-//   // measure(result: FuzzTestResult): number;
-
-//   // measure function
-
-//   // progress
-// }
-
 export abstract class Measure {
   /** Unique measure identifier. */
   static get id(): string {
