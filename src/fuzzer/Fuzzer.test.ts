a10017c84e7a9d7ac765e608ac69a09de5d3dfcd
Matt Davis <22134678+cmumatt@users.noreply.github.com>
2026-03-18 12:22:59 -0400

    chore: refactor compiler

--- src/fuzzer/Fuzzer.test.ts
+++ src/fuzzer/Fuzzer.test.ts
@@ -41,7 +41,7 @@ const intOptions: FuzzOptions = {
   argDefaults: ArgDef.getDefaultOptions(),
   maxTests: 1000,
   fnTimeout: 100,
-  suiteTimeout: 10000,
+  suiteTimeout: 0,
   seed: "qwertyuiop",
   maxDupeInputs: 1000,
   maxFailures: 0,
