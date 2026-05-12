7f5aea460c434890537a5038159ffc69e8f13bae
Matt Davis <22134678+cmumatt@users.noreply.github.com>
2026-05-12 15:56:50 -0400

    chore: add unit tests

--- src/fuzzer/analysis/typescript/ProgramDef.test.ts
+++ src/fuzzer/analysis/typescript/ProgramDef.test.ts
@@ -70,4 +70,26 @@ describe("fuzzer/analysis/typescript/ProgramDef:", () => {
       ProgramDef.fromSource(() => `export {"b" as default};`).getDefaultExport()
     ).toThrow();
   });
+
+  it("Issue #349 parenthesized types", () => {
+    const exportedFunctions = ProgramDef.fromSource(
+      () => `type NumberOrString = number | string;
+      export function test1a(arr: (number | string)[]): void {};
+      export function test2a(a: { b: NumberOrString }): void {};
+      export function test1b(arr: NumberOrString[]): void {};
+      export function test2b(a: { b:  number | string }): void {};`
+    ).getExportedFunctions();
+    expect(
+      exportedFunctions["test1a"].getArgDefs().map((a) => a.getTypeAnnotation())
+    ).toEqual(["(number | string)[]"]);
+    expect(
+      exportedFunctions["test2a"].getArgDefs().map((a) => a.getTypeAnnotation())
+    ).toEqual(["{ b: NumberOrString }"]);
+    expect(
+      exportedFunctions["test1b"].getArgDefs().map((a) => a.getTypeAnnotation())
+    ).toEqual(["NumberOrString[]"]);
+    expect(
+      exportedFunctions["test2b"].getArgDefs().map((a) => a.getTypeAnnotation())
+    ).toEqual(["{ b: number | string }"]);
+  });
 });
