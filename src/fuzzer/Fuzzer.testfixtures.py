227e8ae120ac41ad73a4bbea9d5604d1f929fd1a
Matt Davis <22134678+cmumatt@users.noreply.github.com>
2026-07-20 22:39:03 -0400

    chore: fixtures for property test (commented out)

--- src/fuzzer/Fuzzer.testfixtures.py
+++ src/fuzzer/Fuzzer.testfixtures.py
@@ -1,8 +1,15 @@
+# from typing import Any, Literal, List, TypedDict
+
+
 type a = str
 
 
 def greeting(name: a) -> a:
     return 'Hello ' + name
+
+
+# def greetingValidator(r: FuzzTestResult) -> Literal["pass","fail","unknown"]:
+#    return "pass" if str(r.output).endswith(r.input[0]) else "fail"
 
 
 def timeouts(n: int) -> int:
