31bc13da1210c02f02e7ba89fe5adb3797eac718
Matt Davis <22134678+cmumatt@users.noreply.github.com>
2026-07-27 10:54:10 -0400

    feat: JSONN serializer

---
+++ src/Jsonn.test.ts
@@ -0,0 +1,78 @@
+import * as JSONN from "./Jsonn";
+import { isKeyedObject } from "./Util";
+
+describe("JSONN: ", () => {
+  it("round-trip values", () => {
+    [
+      null,
+      undefined,
+      true,
+      false,
+      Infinity,
+      -Infinity,
+      3,
+      "hello",
+      true,
+      false,
+      {
+        trueValue: true,
+        noValue: undefined,
+        nullValue: null,
+        arrayValue: [
+          null,
+          undefined,
+          true,
+          false,
+          Infinity,
+          -Infinity,
+          3,
+          "hello",
+          true,
+          false,
+        ],
+      },
+      [
+        null,
+        undefined,
+        true,
+        false,
+        Infinity,
+        -Infinity,
+        3,
+        "hello",
+        true,
+        false,
+        { trueValue: true, noValue: undefined, nullValue: null },
+      ],
+    ].forEach((value) => {
+      const roundtripValue = JSONN.parse<typeof value>(JSONN.stringify(value));
+      expect(roundtripValue).toEqual(value);
+
+      // Jasmine ignores keys with an unknown value, so we need an extra check here
+      // to ensure those are preserved by JSONN.
+      if (isKeyedObject(value)) {
+        let key: keyof typeof value;
+        for (key in value) {
+          if (value[key] === undefined && isKeyedObject(roundtripValue)) {
+            expect(key in roundtripValue).toBeTrue();
+            expect(roundtripValue[key]).toBeUndefined();
+          }
+        }
+      }
+    });
+  });
+
+  /* !!!!!!!!!!
+  it("Issue #301", () => {
+    const input = { a: undefined };
+    const serializedInput = JSONN.stringify(input);
+    const roundtripValue = JSONN.parse<object>(serializedInput);
+    expect(serializedInput).toEqual("{a: undefined}");
+    expect("a" in roundtripValue).toBeTrue();
+    expect(isKeyedObject(roundtripValue)).toBeTrue();
+    if (isKeyedObject(roundtripValue)) {
+      expect(roundtripValue["a"]).toBeUndefined();
+    }
+  });
+  */
+});
