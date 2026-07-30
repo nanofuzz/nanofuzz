45667fc70c8465293f9b3503a1bf05d289a4314d
Matt Davis <22134678+cmumatt@users.noreply.github.com>
2026-07-30 14:58:45 -0400

    chore: rename `CoverageMeasure` to `TypescriptCoverageMeasure`

--- src/fuzzer/measures/MeasureFactory.ts
+++ src/fuzzer/measures/MeasureFactory.ts
@@ -1,16 +1,16 @@
-import { ProgramLanguage } from "../analysis/ProgramFactory";
+import { ProgramLanguage } from "../analysis/Types";
 import { AbstractCoverageMeasure } from "./AbstractCoverageMeasure";
 import { AbstractMeasure } from "./AbstractMeasure";
-import { CoverageMeasure } from "./CoverageMeasure";
+import { TypescriptCoverageMeasure } from "./TypescriptCoverageMeasure";
 import { FailedTestMeasure } from "./FailedTestMeasure";
 import { PythonCoverageMeasure } from "./PythonCoverageMeasure";
 
 export function MeasureFactory(lang: ProgramLanguage): AbstractMeasure[] {
   let covMeasure: AbstractCoverageMeasure;
-  if (lang === 'python') {
+  if (lang === "python") {
     covMeasure = new PythonCoverageMeasure();
-  } else { // ts
-    covMeasure = new CoverageMeasure();
+  } else {
+    covMeasure = new TypescriptCoverageMeasure();
   }
   const testMeasure = new FailedTestMeasure(covMeasure);
   return [covMeasure, testMeasure];
