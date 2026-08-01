cc2e2c8fa551a06ddd719cc35a737f215f23896b
Matt Davis <22134678+cmumatt@users.noreply.github.com>
2026-08-01 18:42:48 -0400

    feat: lang-specific `Fuzzer` input dupe check

--- src/fuzzer/Fuzzer.ts
+++ src/fuzzer/Fuzzer.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 import * as JSONN from "../Jsonn";
 import { ArgDef } from "./analysis/ArgDef";
-import { ArgValueType, FunctionRef } from "./analysis/Types";
+import { ArgValueType, FunctionRef, ProgramLanguage } from "./analysis/Types";
 import { CompositeInputGenerator } from "./generators/CompositeInputGenerator";
 import * as CompilerFactory from "./compilers/CompilerFactory";
 import * as ProgramFactory from "./analysis/ProgramFactory";
@@ -36,7 +36,7 @@ export class Tester {
   protected _fnName: string; // function name
   protected _leaderboard = new Leaderboard<InputAndSource>(); // top test results, according to measures
   protected _measures; // set of measures for executions
-  protected _allInputs: Record<string, true> = {}; // dupe check for input generation
+  protected _allInputs: Record<string, true> = {}; // language-specific dupe check for input generation
   protected _state: "init" | "ready" | "running" | "paused" | "crashed" =
     "init"; // tester state
 
@@ -701,22 +701,21 @@ export class Tester {
         }
       }
 
-      // Skip tests if we previously processed the input
-      const inputHash = getIoKey(result.input);
-      if (inputHash in this._allInputs) {
-        runStats.counters.dupesSequential++; // increment the dupe counter
-        runStats.counters.dupesGenerated++; // incremement the total run dupe counter
-        this._compositeInputGenerator.onInputFeedback([], result.timers.gen); // return empty input generator feedback
-        if (genStats) {
-          genStats.counters.dupesGenerated++; // increment the generator's dupe counter
-        }
-
-        continue; // skip this test
-      } else {
-        runStats.counters.dupesSequential = 0; // reset the duplicate count
-        // if the function accepts inputs, add test input
-        // to the list so we don't test it again,
-        if (this._function.getArgDefs().length) {
+      // If the function accepts inputs, check if the input is a dupe
+      if (this._function.getArgDefs().length) {
+        // Skip tests if we previously processed the input
+        // Note the our hash value is language specific
+        const inputHash = getLangIoKey(lang, result.input);
+        if (inputHash in this._allInputs) {
+          runStats.counters.dupesSequential++; // increment the sequential dupe counter
+          runStats.counters.dupesGenerated++; // incremement the total run dupe counter
+          this._compositeInputGenerator.onInputFeedback([], result.timers.gen); // return empty input generator feedback
+          if (genStats) {
+            genStats.counters.dupesGenerated++; // increment the generator's dupe counter
+          }
+          continue; // skip this test
+        } else {
+          runStats.counters.dupesSequential = 0; // reset the sequential duplicate count
           this._allInputs[inputHash] = true;
         }
       }
@@ -1080,6 +1079,22 @@ export function getIoKey(io: FuzzIoElement[]): string {
     })
   );
 } // fn: getIoKey
+
+/**
+ * Gets the langiage-specific input key as a string from an array of `FuzzIoElement`s
+ *
+ * @param `lang` programming language
+ * @param `io` array of `FuzzIoElements`
+ * @returns string representation array of inputs in `lang` format
+ */
+export function getLangIoKey(
+  lang: ProgramLanguage,
+  io: FuzzIoElement[]
+): string {
+  return JSONN.stringify(
+    io.map((input) => ValueMapper.toLang(lang, input.value))
+  );
+} // fn: getLangIoKey
 
 /**
  * Fuzzer Environment required to fuzz a function.
