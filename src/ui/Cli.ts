b9bf13f3cb05150a1ab4beaf0b2340dd9ebe41d0
Matt Davis <22134678+cmumatt@users.noreply.github.com>
2026-08-04 21:23:28 -0400

    feat: cli

--- src/ui/Cli.ts
+++ src/ui/Cli.ts
@@ -1,4 +1,5 @@
 import * as Commander from "commander";
+import * as fs from "node:fs";
 import { Parser } from "web-tree-sitter";
 import { ArgDef, Tester } from "../fuzzer/Fuzzer";
 
@@ -59,15 +60,22 @@ Commander.program
   .option(`--model-name <modelName>`, `AI model name`)
   .option(`--model-key <modelKey>`, `AI model API key`);
 
-// !!!!!!!!!! Pyhon Debug
-// !!!!!!!!!! Set node-llm env variables??
-
 Commander.program.parse();
+
+const filename = Commander.program.args[0];
+const fnname = Commander.program.args[1];
+
+if (!fs.existsSync(Commander.program.args[0])) {
+  console.error(`File not found: ${filename}`);
+  process.exit(4); // command line usage error
+}
+
+// !!!!!!!!!! check for existence of function
 
 const options = Commander.program.opts();
 
 Parser.init().then(async () => {
-  const results = await new Tester("nanofuzz-study/examples/1.ts", "minValue", {
+  const results = await new Tester(filename, fnname, {
     argDefaults: ArgDef.getDefaultOptions(),
     maxTests: options["maxTests"],
     fnTimeout: options["fnTimeout"],
@@ -104,7 +112,7 @@ Parser.init().then(async () => {
 
   if (testsRan) {
     if (testsFailed) {
-      process.exit(); // tests ran and some failed
+      process.exit(1); // tests ran and some failed
     } else {
       process.exit(0); // tests ran and none failed
     }
