0e69af4f874d3f8fbf4be040ab3194631f36833a
Matt Davis <22134678+cmumatt@users.noreply.github.com>
2026-08-10 22:17:34 -0400

    feat: adopt `@given` types

--- src/fuzzer/analysis/Util.ts
+++ src/fuzzer/analysis/Util.ts
@@ -1,3 +1,5 @@
+import { ArgType } from "./Types";
+
 /**
  * Replacer function for JSON.stringify that removes the parent property
  *
@@ -12,3 +14,22 @@ export function removeParents(key: string, value: unknown): unknown {
     return value;
   }
 } // fn: removeParents()
+
+/**
+ * Type guard function that returns true if `obj` is an ArgType
+ *
+ * @param `obj` the object to check
+ * @returns true if `obj` is an ArgType, false otherwise
+ */
+export function isArgType(obj: unknown): obj is ArgType {
+  return (
+    typeof obj === "string" ||
+    typeof obj === "number" ||
+    typeof obj === "boolean" ||
+    (obj !== null &&
+      typeof obj === "object" &&
+      !Array.isArray(obj) &&
+      Object.keys(obj).length > 0 &&
+      Object.values(obj).every((i) => isArgType(i)))
+  );
+} // fn: isArgType
