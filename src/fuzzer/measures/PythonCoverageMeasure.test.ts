f67d88b5c6c57914c04ecf63f4d729442272b827
Matt Davis <22134678+cmumatt@users.noreply.github.com>
2026-09-09 17:35:03 -0400

    fix: python code coverage for validators, transformers (#513)

--- src/fuzzer/measures/PythonCoverageMeasure.test.ts
+++ src/fuzzer/measures/PythonCoverageMeasure.test.ts
@@ -82,12 +82,14 @@ class TestPythonCoverageMeasure extends PythonCoverageMeasure {
     super();
     this._info = { ...staticInfo };
     this._coverage = { [file]: this._info };
-    this._runner = new StubPythonRunner(this._coverage);
+    this._runners = [new StubPythonRunner(this._coverage)];
   }
 
   public record(run: PythonRun): void {
+    this.onBeforeNextTestExecution();
     this._info.lines = run.lines;
     this._info.arcs = run.arcs;
+    this.recordHits(this._coverage);
   }
 
   /**
@@ -877,7 +879,7 @@ describe("fuzzer/analysis/measures/PythonCoverageMeasure:", () => {
         for (const [file, info] of Object.entries(statics)) {
           this._coverage[file] = { ...info };
         }
-        this._runner = new StubPythonRunner(this._coverage);
+        this._runners = [new StubPythonRunner(this._coverage)];
       }
 
       /**
@@ -888,6 +890,7 @@ describe("fuzzer/analysis/measures/PythonCoverageMeasure:", () => {
        * @param `runs` what the call executed, by file
        */
       public record(runs: Record<string, PythonRun>): void {
+        this.onBeforeNextTestExecution();
         for (const [file, info] of Object.entries(this._coverage) as [
           string,
           CoverageInfo,
@@ -895,6 +898,7 @@ describe("fuzzer/analysis/measures/PythonCoverageMeasure:", () => {
           info.lines = runs[file]?.lines;
           info.arcs = runs[file]?.arcs;
         }
+        this.recordHits(this._coverage);
       }
     } // class: MultiFileMeasure
 
