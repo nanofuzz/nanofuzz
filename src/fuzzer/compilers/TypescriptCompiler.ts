8dc15c9888fc2f5c47b39c0e598b26268caf8c56
Matt Davis <22134678+cmumatt@users.noreply.github.com>
2026-09-22 23:08:50 -0400

    fix: compile cache

--- src/fuzzer/compilers/TypescriptCompiler.ts
+++ src/fuzzer/compilers/TypescriptCompiler.ts
@@ -926,12 +926,7 @@ const defaultOptions: CompilerOptions = {
   target: "ES2022", // default to ES2022
   moduleKind: "nodenext", // cjs is required for running inside express
   emitOnError: false, // fail compilation in case of errors
-  tmpDir: path.join(
-    fs.realpathSync(os.tmpdir()),
-    "nanofuzz",
-    "tsc",
-    String(process.pid)
-  ), // path for compiled files
+  tmpDir: path.join(fs.realpathSync(os.tmpdir()), "nanofuzz", "tsc"), // path for compiled files
   lib: ["DOM", "ScriptHost", "ES2020", "ES2021.String", "ES2022"], // default to ES2020
   types: [""], // do not automatically import types
   typeRoots: [], // do not automatically import types
